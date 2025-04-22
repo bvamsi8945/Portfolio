196d1465d26079d08579e45a3f66bf20e6eb2aea
Balavamsi <118984754+bvamsi8945@users.noreply.github.com>
2025-04-22 14:49:00 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,15 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: '',
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-    rollupOptions: {
-      input: {
-        main: './index.html'
-      }
-    }
-  }
+  base: './',
 })
+
